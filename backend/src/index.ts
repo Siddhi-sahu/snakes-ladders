5dd147746697c8208a390b512e7ed8dce5bc9a1e
Siddhi Sahu <himanisahu739@gmail.com>
2025-03-03 14:46:44 +0530

    add server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,16 @@
 //entery file minimal code
+//ws library for backend
+import { WebSocketServer } from "ws";
 
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('received:', data);
+
+    });
+
+    ws.send('seomthing');
+})
