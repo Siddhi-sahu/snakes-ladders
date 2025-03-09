04fe81f55fc7976e8a91aa9706b6522f04755379
Siddhi Sahu <himanisahu739@gmail.com>
2025-03-09 22:20:54 +0530

    add user join logic

--- backend/src/Game.ts
+++ backend/src/Game.ts
@@ -1,3 +1,9 @@
+import { WebSocket } from "ws";
+
 export class Game {
+
+    constructor(player1: WebSocket, player2: WebSocket) {
+
+    }
 
 }
--- backend/src/GameManager.ts
+++ backend/src/GameManager.ts
@@ -1,19 +1,39 @@
 //multiple games
 
 import { WebSocket } from "ws";
+import { Game } from "./Game";
+import { INIT_GAME } from "./messages";
 
+//two player will get 2 different game class? first pU = null then a user joins and 
+//Every time a new GameManager is created, a new instance of the class is made but the static variables (like pendingUser) persist across all instances.
+// In this case, this.pendingUser is shared among all incoming users because it is not local to the constructor.
 export class GameManager {
     public player;
-    public pendingUser: WebSocket;
+    public pendingUser: WebSocket | null;
+    public GAMES = [];
     constructor(socket: WebSocket) {
         this.player = socket;
+        this.pendingUser = null;
+        this.init(socket);
 
     };
 
-    init() {
+    //Games array contains all games instances
+
+
+    init(socket: WebSocket) {
         if (this.pendingUser) {
+            const game = new Game(socket, this.pendingUser);
             //start the game
             //emit init game message
+
+            // this.GAMES.push(game)
+
+            socket.emit(JSON.stringify({
+                type: INIT_GAME
+            }));
+
+            this.pendingUser = null;
 
         } else {
             this.pendingUser = this.player;
